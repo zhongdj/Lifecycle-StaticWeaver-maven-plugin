c07dfdeb4e6400fdd557c06cf4ee872d6e758a5f
Barry <zhongdj@gmail.com>
2015-04-24 14:04:53 +0800

    fix jdk8 process not exit issue

--- src/main/java/net/imadz/lifecycle/maven/LifecycleStaticWeaver.java
+++ src/main/java/net/imadz/lifecycle/maven/LifecycleStaticWeaver.java
@@ -34,16 +34,16 @@
  */
 package net.imadz.lifecycle.maven;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
-
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 
 /**
  * @goal StaticWeave
@@ -54,70 +54,81 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class LifecycleStaticWeaver extends AbstractMojo {
 
-	/**
-	 * @parameter expression="${project.build.directory}"
-	 */
-	private String buildDir;
+    /**
+     * @parameter expression="${project.build.directory}"
+     */
+    private String buildDir;
+    /**
+     * @parameter expression="${lifecycle.path}"
+     */
+    private String lifecyclePath;
+    /**
+     * @parameter expression="${project.runtimeClasspathElements}"
+     */
+    private List<String> runtimeClasspathElements;
 
-	/**
-	 * @parameter expression="${lifecycle.path}"
-	 */
-	private String lifecyclePath;
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info(new File("./").getAbsolutePath());
+        final String separator;
+        if ( File.separatorChar == '\\' ) {
+            separator = "\\";
+        } else {
+            separator = File.separator;
+        }
+        final String targetClassesFolder = buildDir + separator + "classes";
+        getLog().info(targetClassesFolder);
+        getLog().info(lifecyclePath);
+        final StringBuffer classpath = new StringBuffer(".:");
+        for ( String element : runtimeClasspathElements ) {
+            classpath.append(element).append(":");
+        }
+        classpath.append(targetClassesFolder).append(":");
+        final File lifecycleJar = new File(lifecyclePath);
+        classpath.append(lifecycleJar);
+        getLog().info(classpath);
+        String cmd = "java -cp " + classpath + " -javaagent:" + lifecyclePath + " -Dnet.imadz.bcel.save.original=true" + " "
+            + "net.imadz.lifecycle.StaticWeaver " + targetClassesFolder;
+        getLog().info(cmd);
+        BufferedReader reader = null;
+        Process exec = null;
+        try {
+            exec = Runtime.getRuntime().exec(cmd);
+            InputStream errorStream = exec.getErrorStream();
+            reader = new BufferedReader(new InputStreamReader(errorStream));
+            String line = null;
+            while ( null != ( line = reader.readLine() ) ) {
+                getLog().info(line);
+            }
+            while ( isAlive(exec) ) {
+                Thread.sleep(500);
+            }
+            if (  exec.exitValue() > 0 ) {
+                throw new IllegalStateException("Lifecycle Cannot Compile.");
+            }
+        } catch ( IOException e ) {
+            getLog().error(e);
+        } catch ( InterruptedException e ) {
+            e.printStackTrace();
+        } finally {
+            if ( null != reader ) {
+                try {
+                    reader.close();
+                } catch ( IOException e ) {
+                }
+            }
+            if ( null != exec ) {
+                exec.destroy();
+            }
+        }
+    }
 
-	/**
-	 * @parameter expression="${project.runtimeClasspathElements}"
-	 */
-	private List<String> runtimeClasspathElements;
+    private Boolean isAlive(Process exec) {
+        try {
+             exec.exitValue();
+            return false;
+        } catch (Throwable t) {
+            return true;
+        }
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
-
-		getLog().info(new File("./").getAbsolutePath());
-
-		final String separator;
-		if (File.separatorChar == '\\') {
-			separator = "\\";
-		} else {
-			separator = File.separator;
-		}
-		final String targetClassesFolder = buildDir + separator + "classes";
-		getLog().info(targetClassesFolder);
-		getLog().info(lifecyclePath);
-		final StringBuffer classpath = new StringBuffer(".:");
-		for (String element : runtimeClasspathElements) {
-			classpath.append(element).append(":");
-		}
-		classpath.append(targetClassesFolder).append(":");
-
-		final File lifecycleJar = new File(lifecyclePath);
-		classpath.append(lifecycleJar);
-		getLog().info(classpath);
-
-		String cmd = "java -cp " + classpath + " -javaagent:" + lifecyclePath
-				+ " -Dnet.imadz.bcel.save.original=true" + " "
-				+ "net.imadz.lifecycle.StaticWeaver " + targetClassesFolder;
-		getLog().info(cmd);
-		BufferedReader reader = null;
-		try {
-			Process exec = Runtime.getRuntime().exec(cmd);
-			InputStream errorStream = exec.getErrorStream();
-			reader = new BufferedReader(new InputStreamReader(errorStream));
-			String line = null;
-			while (null != (line = reader.readLine())) {
-				getLog().info(line);
-			}
-			if (exec.exitValue() > 0) {
-				throw new IllegalStateException("Lifecycle Cannot Compile.");
-			}
-		} catch (IOException e) {
-			getLog().error(e);
-		} finally {
-			if (null != reader) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-	}
-
+    }
 }
